36bb602b1f1a52a6ad859aadfbfc7460b1dce097
asreed666 <areed@cityplym.ac.uk>
2022-12-13 11:51:04 +0000

    more tidying up

--- src/lightLevel.cpp
+++ src/lightLevel.cpp
@@ -6,11 +6,9 @@ extern things_t myData;
 
 
 AnalogIn lightLevelV( lightOut );
-DigitalOut test(P12_3);
 void readLight() {
   while(true) {
     myData.lightLevel = lightLevelV.read() * 100; // Range of light level 0 to 100%
     ThisThread::sleep_for(100ms);
-    test = !test;
   }
 }
