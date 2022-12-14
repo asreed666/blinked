66c86ec1e6c6570a56e6ca03a896ddeb00e6f511
asreed666 <areed@cityplym.ac.uk>
2022-12-14 09:05:30 +0000

    timing

--- src/display.cpp
+++ src/display.cpp
@@ -8,7 +8,7 @@ void displayTask() {
     DigitalOut green(GREENLED);
 
     while(true) {
-        ThisThread::sleep_for(100ms);
+        ThisThread::sleep_for(500ms);
         printf("The button is %s\n", myData.buttonState?"pressed":"not pressed");
         if (myData.buttonState == true) green = true;
         else green = false;
