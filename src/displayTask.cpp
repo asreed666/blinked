dc7d0283129e96a44b05482d2e83679513d54fd7
asreed666 <areed@cityplym.ac.uk>
2023-01-03 12:24:50 +0000

    VT100 Screen Control

--- src/displayTask.cpp
+++ src/displayTask.cpp
@@ -2,18 +2,22 @@
 #include "stdio.h"
 #include "constants.h"
 #include "displayTask.h"
+#include "vt100.h"
 
 extern things_t myData;
-
 void displayTask() {
 /* setup */
     int period = 0;
     DigitalOut green(greenLed);
+    RIS; // Reset the VT100 terminal to text mode
+    ThisThread::sleep_for(1s);
     while(true){ /* loop forever */
         ThisThread::sleep_for(100ms);
         green = myData.switchState;
         if (period++ > 4) {
-            printf("Switch %s Pressed\n", myData.switchState?"is":"is not");
+            HOME; // put cursor in position 1,1
+            printf("Led is %s\n", myData.switchState?"\033[0;31mOn \033[0;37m":"\033[0;32mOff\033[0;37m");
+
             printf("Temperature is: %2.1fC\n", myData.temperature);
             printf("Light is at %3.0f%c\n", myData.lightLevel, '%');
             period = 0;
