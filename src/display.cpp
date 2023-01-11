1106b978d384ca87a01756aab714061927fe8fb7
asreed666 <areed@cityplym.ac.uk>
2023-01-11 10:02:23 +0000

    Updated for Wed-11th-Jan

--- src/display.cpp
+++ src/display.cpp
@@ -7,30 +7,27 @@ extern things_t myData;
 
 void displayTask() {
     DigitalOut green(GREENLED);
-    RIS;  // reset vt100 terminal
-    ThisThread::sleep_for(5000ms);
-    HIDE_CURSOR;
-    BOLD;
-    
-    printf("\033[1;20HEnvironmental Sensing");
-    NORMAL;
-    while(true) {
-        ThisThread::sleep_for(500ms);
-    //    HOME; // VT100 Home the cursor
-        CYAN_TEXT;
-        printf("\033[8;3HThe LED is %s\n", myData.buttonState?"\033[1;32mOn ":"\033[2;32mOff");
-        if (myData.buttonState == true) {
-            BLUE_BOLD;
-            green = true;
-        }
-        else {
-            green = false;
-            BLUE_TEXT;
-        }
- 
-        printf("\033[10;3HTemperature: %2.1fC \n", myData.tempC);
-        GREEN_TEXT;
-        printf("\033[12;3HLight Level: %3.0f%c \n", myData.lightL, '%');
+    printf("\033[2J");  // Clear the terminal display
+    ThisThread::sleep_for(1000ms);
+    printf("\033[?25l"); // Hide the Visible Cursor
+
+    printf("\033[1;20H\033[1;37mEnvironmental Sensing\033[0;37m");
+    ThisThread::sleep_for(100ms);
+
+    while (true) {
+      ThisThread::sleep_for(500ms);
+      printf("\033[8;3HThe LED is %s\n",
+             myData.buttonState ? "\033[1;32mOn \033[0;37m" 
+                                : "\033[2;32mOff\033[0;37m");
+      if (myData.buttonState == true) {
+        green = true;
+      } else {
+        green = false;
+      }
+      ThisThread::sleep_for(1ms);
+      printf("\033[10;3HTemperature: %2.1fC \n", myData.tempC);
+      ThisThread::sleep_for(500ms);
+      printf("\033[12;3HLight Level: %3.0f%c \n", myData.lightL, '%');
     }
 
 }
