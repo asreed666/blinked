19e82e485669c9240036b2d7fa22c40ba1b36076
asreed666 <areed@cityplym.ac.uk>
2023-01-24 12:48:11 +0000

    added config defines

---
+++ src/config.h
@@ -0,0 +1,18 @@
+#ifndef CONFIG_WIFI
+#define CONFIG_WIFI
+
+#define thingName "asrThang"
+#define mqttBroker "192.168.1.176"
+#define LIGHT_LEVEL_SET_TOPIC "ASR_Thang/lightSet"
+#define LIGHT_LEVEL_TOPIC "/lightLevel"
+#define ANNOUNCE_TOPIC "announce"
+#define TEMP_SET_TOPIC "/tempSet"
+#define TEMPERATURE_SET_TOPIC "ASR_Thang/tempSet"
+#define TEMPERATURE_TOPIC "/temp"
+#define GET_TIME_TOPIC "getTime"
+#define TIME_TOPIC "timeStamp"
+#define MQTTClient_QOS2 1
+#define LEDON 0
+#define LEDOFF 1
+
+#endif
