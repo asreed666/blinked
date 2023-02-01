53fb5feec9c2ca046a1fbfca633d987f77893c15
asreed666 <areed@cityplym.ac.uk>
2023-02-01 08:48:03 +0000

    Changed switch port

--- src/constants.h
+++ src/constants.h
@@ -5,7 +5,7 @@
 #define thermOut P10_1
 #define lightOut P10_4
 #define switchPort P10_3
-#define greenLed P12_3
+#define greenLed P0_5
 
 /* Reference resistor in series with the thermistor is of 10kohm */
 #define R_REFERENCE                         (float)(10000)
