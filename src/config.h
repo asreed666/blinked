42f1c8e7c8f3720c730638e6bab2e8ff7fe325ff
asreed666 <areed@cityplym.ac.uk>
2023-02-22 10:15:16 +0000

    Adding MQTT communication

---
+++ src/config.h
@@ -0,0 +1,17 @@
+#ifndef CONFIG
+#define CONFIG
+/* MQTT configuration file
+ *
+ * Set your unique THING_NAME
+*/
+
+#define BROKER          "192.168.1.176"
+#define PORT            1883
+
+#define THING_NAME      "asr_thing"
+
+#define TEMPERATURE_TOPIC   "/temperatureC"
+#define LIGHT_LEVEL_TOPIC   "/lightlevel"
+#define ANNOUNCE_TOPIC      "/announce"
+
+#endif
--- src/wifiTask.cpp
+++ src/wifiTask.cpp
@@ -16,6 +16,8 @@
 
 #include "mbed.h"
 #include "wifiTask.h"
+#include "config.h"
+#include "MQTTClientMbedOs.h"
 #include "wifi_helper.h"
 #include "mbed-trace/mbed_trace.h"
 
@@ -59,7 +61,7 @@ public:
         /* if we're using a wifi interface run a quick scan */
         if (_net->wifiInterface()) {
             /* the scan is not required to connect and only serves to show visible access points */
-            //wifi_scan();
+            //wifi_scan(); */
 
             /* in this example we use credentials configured at compile time which are used by
              * NetworkInterface::connect() but it's possible to do this at runtime by using the
@@ -85,168 +87,64 @@ public:
             printf("Error! _socket.open() returned: %d\r\n", result);
             return;
         }
-
-#if MBED_CONF_APP_USE_TLS_SOCKET
-        result = _socket.set_root_ca_cert(root_ca_cert);
-        if (result != NSAPI_ERROR_OK) {
-            printf("Error: _socket.set_root_ca_cert() returned %d\n", result);
-            return;
+        printf("Starting MQTT Reporting to %S\n", (wchar_t *)BROKER);
+        char buffer[80];
+        char topicBuffer[80];
+        uint32_t rc;
+        uint32_t failure = 0;
+        MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+        data.clientID.cstring = (char *)THING_NAME;
+        data.keepAliveInterval = 20;
+        data.cleansession = 1;
+        data.username.cstring = (char *)"";
+        data.password.cstring = (char *)"";
+        char *host = (char *)BROKER;
+        uint16_t port = PORT;
+        TCPSocket socket;
+        MQTTClient client(&socket);
+        socket.open(_net);
+        rc = socket.connect(host, port);
+        if (rc == 0) {
+            printf("Succesful connection of socket to Host %s port %d\n", host, port);
+        } else {
+            printf("Socket connection failed");
+            while (socket.connect(host, port)) {
+                printf(".");
+            }
         }
-        _socket.set_hostname(MBED_CONF_APP_HOSTNAME);
-#endif // MBED_CONF_APP_USE_TLS_SOCKET
+        rc = client.connect(data);
+        if (rc == 0) {
+            printf("Succesful connection of %s to Broker\n", data.clientID.cstring);
+        } else {
+            printf("Client connection failed");
+        }
+        MQTT::Message message{};
+        sprintf(buffer, "Hello World! from %s\r\n", THING_NAME);
+        message.qos = MQTT::QOS0;
+        message.retained = false;
+        message.dup = false;
+        message.payload = (void *)buffer;
+        message.payloadlen = strlen(buffer) + 1;
 
-        /* now we have to find where to connect */
+        rc = client.publish(ANNOUNCE_TOPIC, message);
+        if (rc == 0) {
+            printf("publish announce worked\n");
+            }
+             
+        else {
+            printf("publish announce failed %d\n", rc);
+        }
 
- //       SocketAddress address;
-
- //       if (!resolve_hostname(address)) {
- //           return;
- //       }
-
- //       address.set_port(REMOTE_PORT);
-
-        /* we are connected to the network but since we're using a connection oriented
-         * protocol we still need to open a connection on the socket */
-
- //       printf("Opening connection to remote port %d\r\n", REMOTE_PORT);
-
- //       result = _socket.connect(address);
- //       if (result != 0) {
- //           printf("Error! _socket.connect() returned: %d\r\n", result);
- //           return;
- //       }
-
-        /* exchange an HTTP request and response */
-
- //       if (!send_http_request()) {
- //           return;
- //       }
-
- //       if (!receive_http_response()) {
- //           return;
- //       }
-
- //       printf("Demo concluded successfully \r\n");
     }
 
 private:
-    /*
-    bool resolve_hostname(SocketAddress &address)
-    {
-        const char hostname[] = MBED_CONF_APP_HOSTNAME;
-
-        /* get the host address */
-        /*
-        printf("\nResolve hostname %s\r\n", hostname);
-        nsapi_size_or_error_t result = _net->gethostbyname(hostname, &address);
-        if (result != 0) {
-            printf("Error! gethostbyname(%s) returned: %d\r\n", hostname, result);
-            return false;
-        }
-
-        printf("%s address is %s\r\n", hostname, (address.get_ip_address() ? address.get_ip_address() : "None") );
-
-        return true;
-    }*/
-
-    /*
-    bool send_http_request()
-    {
-        /* loop until whole request sent */
-    /*
-        const char buffer[] = "GET / HTTP/1.1\r\n"
-                              "Host: ifconfig.io\r\n"
-                              "Connection: close\r\n"
-                              "\r\n";
-
-        nsapi_size_t bytes_to_send = strlen(buffer);
-        nsapi_size_or_error_t bytes_sent = 0;
-
-        printf("\r\nSending message: \r\n%s", buffer);
-
-        while (bytes_to_send) {
-            bytes_sent = _socket.send(buffer + bytes_sent, bytes_to_send);
-            if (bytes_sent < 0) {
-                printf("Error! _socket.send() returned: %d\r\n", bytes_sent);
-                return false;
-            } else {
-                printf("sent %d bytes\r\n", bytes_sent);
-            }
-
-            bytes_to_send -= bytes_sent;
-        }
-
-        printf("Complete message sent\r\n");
-
-        return true;
-    }
-
-    bool receive_http_response()
-    {
-        char buffer[MAX_MESSAGE_RECEIVED_LENGTH];
-        int remaining_bytes = MAX_MESSAGE_RECEIVED_LENGTH;
-        int received_bytes = 0;
-
-        /* loop until there is nothing received or we've ran out of buffer space */
-        /*
-        nsapi_size_or_error_t result = remaining_bytes;
-        while (result > 0 && remaining_bytes > 0) {
-            result = _socket.recv(buffer + received_bytes, remaining_bytes);
-            if (result < 0) {
-                printf("Error! _socket.recv() returned: %d\r\n", result);
-                return false;
-            }
-
-            received_bytes += result;
-            remaining_bytes -= result;
-        }
-
-        /* the message is likely larger but we only want the HTTP response code */
-        /*
-
-        printf("received %d bytes:\r\n%.*s\r\n\r\n", received_bytes, strstr(buffer, "\n") - buffer, buffer);
-
-        return true;
-    }
-
-    void wifi_scan()
-    {
-        WiFiInterface *wifi = _net->wifiInterface();
-
-        WiFiAccessPoint ap[MAX_NUMBER_OF_ACCESS_POINTS];
-
-        /* scan call returns number of access points found */
-        /*
-        int result = wifi->scan(ap, MAX_NUMBER_OF_ACCESS_POINTS);
-
-        if (result <= 0) {
-            printf("WiFiInterface::scan() failed with return value: %d\r\n", result);
-            return;
-        }
-
-        printf("%d networks available:\r\n", result);
-
-        for (int i = 0; i < result; i++) {
-            printf("Network: %s secured: %s BSSID: %hhX:%hhX:%hhX:%hhx:%hhx:%hhx RSSI: %hhd Ch: %hhd\r\n",
-                   ap[i].get_ssid(), get_security_string(ap[i].get_security()),
-                   ap[i].get_bssid()[0], ap[i].get_bssid()[1], ap[i].get_bssid()[2],
-                   ap[i].get_bssid()[3], ap[i].get_bssid()[4], ap[i].get_bssid()[5],
-                   ap[i].get_rssi(), ap[i].get_channel());
-        }
-        printf("\r\n");
-    }
-    */
-
+ 
     void print_network_info()
     {
         /* print the network info */
         SocketAddress a;
         _net->get_ip_address(&a);
         printf("\033[5;1HIP address: %s", a.get_ip_address() ? a.get_ip_address() : "None");
-//        _net->get_netmask(&a);
-//        printf("\033[5;1HNetmask: %s", a.get_ip_address() ? a.get_ip_address() : "None");
-//        _net->get_gateway(&a);
-//        printf("\033[6;1HGateway: %s", a.get_ip_address() ? a.get_ip_address() : "None");
     }
 
 private:
